6e07443cadb718f472cc9318355e5c5bc792a69d
ItsAndreaCaglioti <andrea.caglioti@edu.itspiemonte.it>
2022-06-30 14:46:23 +0200

    add

---
+++ html/orienta.js
@@ -0,0 +1,19 @@
+let obj_quiz=[
+    ["Quale influencer segui per interesse verso i temi trattati?",
+        "Salvatore Aranzulla", 
+        "Science Cahnnel",
+        "Greta Thunberg",
+        "Chiara ferragni",
+        "Fabio Wibmer",
+        "Human Safari"
+    ],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+];
+
