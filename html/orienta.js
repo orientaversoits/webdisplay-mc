cfdde69576c8ee7886d93ebf5c506273fb424070
ItsAndreaCaglioti <andrea.caglioti@edu.itspiemonte.it>
2022-06-30 16:11:45 +0200

    add

--- html/orienta.js
+++ html/orienta.js
@@ -6,13 +6,68 @@ let obj_quiz=[
         ["Creazione siti web", "a"],
         ["Programmazione", "a"]
     ],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
+    [      
+        "Come ti senti quando devi parlare in pubblico?",
+        ["Ho il terrore di parlare in pubblico, se posso evito", "a"],
+        ["Sono agitato e non riesco a presentare come vorrei", "a"],
+        ["Un po' nervoso ma riesco comunque a presentare bene", "a"],
+        ["Non ho difficoltà a parlare in pubblico", "a"]
+    ],
+    [        
+        "Per cosa usi maggiormente i social?",
+        ["Sì, per intrattenimento", "a"],
+        ["Sì, per informarmi", "a"],
+        ["Sì, per condividere contenuti", "a"],
+        ["No, non mi interessano", "a"]
+    ],
+    [        
+        "Quando bisogna organizzare qualcosa, come ti comporti?",
+        ["Organizzo e gestisco tutto da solo.", "a"],
+        ["Sono molto neutrale nelle scelte, mi adeguo a quello che decidono gli altri.", "a"],
+        ["Ascolto ciò che gli altri propongono  e propongo delle alternative o soluzioni a mia volta.", "a"],
+        ["Sono indifferente, l'importante è divertirsi.", "a"]
+    ],
+    [   
+        "In quale di queste situazioni ti identifichi di più?",
+        ["Organizzo", "a"],
+        ["Lavoro, i risultati arriveranno", "a"],
+        ["Raccolgo informazioni", "a"],
+        ["Mi interesso agli altri", "a"]
+    ],
+    [   
+        "Cosa fai quando c'è un compleanno di un tuo amico?",
+        ["Gli realizzo un biglietto personalizzato", "a"],
+        ["Organizzo la colletta per il regalo tra tutti gli amici", "a"],
+        ["Scrivo una lettera di auguri", "a"],
+        ["Controllo la lista dei regali che gli ho già fatto", "a"]
+    ],
+    [     
+        "Quando vedi una pubblicità?",
+        ["Studio il contesto nel quale è inserita", "a"],
+        ["Mi soffermo sui testi", "a"],
+        ["Analizzo la grafica", "a"],
+        ["In genere, ignoro le pubblicità", "a"]
+    ],
+    [  
+        "Quale personalità ti si addice maggiormente?",
+        ["Logico", "a"],
+        ["Creativo", "a"],
+        ["Intraprendente", "a"],
+        ["Curioso", "a"]
+    ],
+    [   
+        "Hai mai provato a creare storie o poesie?",
+        ["Sì, provo a creare storie da vendere.", "a"],
+        ["No, non mi interessa scrivere.", "a"],
+        ["Sì, per intrattenere le persone.", "a"],
+        ["Sì, le scrivo per me stesso, per buttare giù i pensieri.", "a"]
+    ],
+    [   
+        "Quando ti ritrovi ad un evento sociale",
+        ["Cerco di capire chi sono i personaggi di spicco.", "a"],
+        ["Tendo ad allontanarmi dalla calca.", "a"],
+        ["Provo a conoscere più persone possibili.", "a"],
+        ["Studio le dinamiche relazionali tra le persone.", "a"]
+    ]
 ];
 
