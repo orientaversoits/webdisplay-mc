923f7168cebe796a6217fa5ec73318b5845ef2ea
ItsAndreaCaglioti <andrea.caglioti@edu.itspiemonte.it>
2022-07-04 15:50:06 +0200

    add

--- io/script.js
+++ io/script.js
@@ -1,14 +1,11 @@
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "id19192937_orientaverso",
-  user: "id19192937_orientaverso_",
-  password: "2UoMPLbkimnuo$vLuru!"
-});
-
-con.connect(function(err) {
-    let out;
-  if (err) out=err;//throw err;
-  else out="Connected" 
-  document.getElementById("sql").innerText=out;
-});
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("try").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "https://orientaverso.000webhostapp.com/php/insert.php");
+    xhttp.send();
+  }
